6cf430742085b74a3746e60b9deabbc3f318cbaa
Compi-Craft <116712858+Compi-Craft@users.noreply.github.com>
2022-11-05 01:14:06 +0200

    Create del_node.py

---
+++ del_node.py
@@ -0,0 +1,10 @@
+def del_node(graph, node):
+    """ 
+    (dict, int) -> (dict)
+    
+    Delete a node and all incident edges from the graph.
+    
+    >>> del_node({1: [2, 5], 2: [1, 4], 3: [4], 4: [2, 3], 5: [1]}, 4)
+    {1: [2, 5], 2: [1], 3: [], 5: [1]}
+    """
+    pass
