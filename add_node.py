eb1de8b7966ea923e9654b034cf5bc43e558f3a0
Compi-Craft <ozarko.pn@ucu.edu.ua>
2022-11-05 01:12:01 +0200

    add_node created

--- add_node.py
+++ add_node.py
@@ -1,10 +1,12 @@
-def add_node(graph, node):
-    """ 
+"""
+Adds node to graph
+"""
+def add_node(graph: dict, node: int) -> dict:
+    """
     (dict, int) -> (dict)
-    
     Add a new node to the graph and return a new graph.
-    
     >>> add_node({1: [2], 2: [1]}, 3)
     {1: [2], 2: [1], 3: []}
     """
-    pass
+    graph.update({node: []})
+    return graph
