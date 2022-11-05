1d6a6c2f64bf74214939dc3fd02af3b1431e17c2
Compi-Craft <ozarko.pn@ucu.edu.ua>
2022-11-05 02:33:57 +0200

    feew changes

--- del_edge.py
+++ del_edge.py
@@ -8,6 +8,8 @@ def del_edge(graph: dict, edge: tuple) -> dict:
     >>> del_edge({1: [2, 5], 2: [1, 4], 3: [4], 4: [2, 3], 5: [1]}, (2, 4))
     {1: [2, 5], 2: [1], 3: [4], 4: [3], 5: [1]}
     """
-    graph[edge[0]].remove(edge[1])
-    graph[edge[1]].remove(edge[0])
+    if edge[1] in graph[edge[0]]:
+        graph[edge[0]].remove(edge[1])
+    if edge[0] in graph[edge[1]]:
+        graph[edge[1]].remove(edge[0])
     return graph
