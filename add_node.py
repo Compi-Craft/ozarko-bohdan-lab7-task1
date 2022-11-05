a8ce27f391cd4f5c3e5b9f1ef1fc697f872e9985
Compi-Craft <ozarko.pn@ucu.edu.ua>
2022-11-05 02:37:23 +0200

    upgraded

--- add_node.py
+++ add_node.py
@@ -8,5 +8,6 @@ def add_node(graph: dict, node: int) -> dict:
     >>> add_node({1: [2], 2: [1]}, 3)
     {1: [2], 2: [1], 3: []}
     """
-    graph.update({node: []})
+    if node not in graph.keys():
+        graph.update({node: []})
     return graph
