2905cf8aff5967d07719dc391d232c190241bba0
Compi-Craft <ozarko.pn@ucu.edu.ua>
2022-11-05 02:36:02 +0200

    more mod

--- graph.py
+++ graph.py
@@ -63,6 +63,18 @@ def add_edge(graph: dict, edge: tuple) -> dict:
         graph.update({edge[1]: []})
     graph[edge[1]].append(edge[0])
     return graph
+def del_edge(graph: dict, edge: tuple) -> dict:
+    """
+    (dict, tuple) -> (dict)
+    Delete an edge from the graph and return a new graph.
+    >>> del_edge({1: [2, 5], 2: [1, 4], 3: [4], 4: [2, 3], 5: [1]}, (2, 4))
+    {1: [2, 5], 2: [1], 3: [4], 4: [3], 5: [1]}
+    """
+    if edge[1] in graph[edge[0]]:
+        graph[edge[0]].remove(edge[1])
+    if edge[0] in graph[edge[1]]:
+        graph[edge[1]].remove(edge[0])
+    return graph
 def add_node(graph: dict, node: int) -> dict:
     """
     (dict, int) -> (dict)
