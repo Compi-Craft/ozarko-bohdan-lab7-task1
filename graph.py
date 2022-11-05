28458e254690afea6370b9de5f79ebc6b4bc6c93
Compi-Craft <ozarko.pn@ucu.edu.ua>
2022-11-05 02:38:45 +0200

    one more update

--- graph.py
+++ graph.py
@@ -82,7 +82,8 @@ def add_node(graph: dict, node: int) -> dict:
     >>> add_node({1: [2], 2: [1]}, 3)
     {1: [2], 2: [1], 3: []}
     """
-    graph.update({node: []})
+    if node not in graph.keys():
+        graph.update({node: []})
     return graph
 def del_node(graph: dict, node: int) -> dict:
     """
