9a42b0079598b2ee8178360f06dff9751ba72448
Compi-Craft <ozarko.pn@ucu.edu.ua>
2022-11-05 02:29:42 +0200

    another

--- graph.py
+++ graph.py
@@ -56,16 +56,6 @@ def add_edge(graph: dict, edge: tuple) -> dict:
     >>> add_edge({1: [2, 5], 2: [1, 4], 3: [4], 4: [2, 3], 5: [1]}, (1, 3))
     {1: [2, 5, 3], 2: [1, 4], 3: [4, 1], 4: [2, 3], 5: [1]}
     """
-    graph[edge[0]].append(edge[1])
-    graph[edge[1]].append(edge[0])
-    return graph
-def add_edge(graph: dict, edge: tuple) -> dict:
-    """
-    (dict, tuple) -> dict
-    Add a new edge to the graph and return new graph.
-    >>> add_edge({1: [2, 5], 2: [1, 4], 3: [4], 4: [2, 3], 5: [1]}, (1, 3))
-    {1: [2, 5, 3], 2: [1, 4], 3: [4, 1], 4: [2, 3], 5: [1]}
-    """
     if edge[0] not in graph:
         graph.update({edge[0]: []})
     graph[edge[0]].append(edge[1])
